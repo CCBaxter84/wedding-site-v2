e1de8ab121868311831b9952289e410a11c9df3f
CCBaxter84 <troyceclark@gmail.com>
2024-03-18 08:47:08 -0600

    Renamed getAllLinks to getPuppyLinks

--- functions/getPuppyLinks.ts
+++ functions/getPuppyLinks.ts
@@ -1,7 +1,10 @@
 import { Handler } from '@netlify/functions'
 import { makeHandlerByQuery } from './utils'
 
-const query = "Link.all().paginate(1000)"
+const query = "Link.all()" +
+  ".where(.description.includes('Nova') ||" +
+    ".description.includes('Revan'))" +
+  ".paginate(1000)"
 const handler: Handler = makeHandlerByQuery(query)
 
 export { handler }
