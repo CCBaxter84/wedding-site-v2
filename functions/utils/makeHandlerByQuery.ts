a7dd31ddc87c4329c896b9b8cb88b99ca3fdb359
CCBaxter84 <troyceclark@gmail.com>
2024-03-18 08:22:34 -0600

    Restored package type to module

--- functions/utils/makeHandlerByQuery.ts
+++ functions/utils/makeHandlerByQuery.ts
@@ -1,5 +1,5 @@
-import getContentByQuery from './getContentByQuery.mts'
-import formatResponse from './formatResponse.mts'
+import getContentByQuery from './getContentByQuery'
+import formatResponse from './formatResponse'
 
 export const makeHandlerByQuery = (query) => async () => {
   try {
