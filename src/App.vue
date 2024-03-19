47caccccf81e505a218f20d8c3a5c5e11cd8ba5c
CCBaxter84 <troyceclark@gmail.com>
2024-03-18 21:07:37 -0600

    Initial styling & App skeleton

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,22 @@
 </script>
 
 <template>
-  <div>App</div>
+  <div class="container has-text-primary">
+    <header class="flex justify-content-center">Navbar goes here</header>
+    <main class="flex justify-content-center">Content goes here</main>
+  </div>
 </template>
 
 <style scoped>
-
+  .container {
+    background-color: hsla(0, 0%, 100%, .5);
+    border-radius: 0.75rem;
+    height: 100vh;
+    margin: 2rem auto;
+    width: 70%;
+  }
+  
+  header {
+    padding: 2rem;
+  }
 </style>
