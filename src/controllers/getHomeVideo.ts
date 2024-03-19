463bd18a773955c5905fd5080665099dca421066
CCBaxter84 <troyceclark@gmail.com>
2024-03-19 08:46:40 -0600

    Added index to types & type to getHomeVideo

--- src/controllers/getHomeVideo.ts
+++ src/controllers/getHomeVideo.ts
@@ -1,6 +1,7 @@
 import http from '@/http'
+import { Link } from '@/types'
 
-export async function getHomeVideo() {
+export async function getHomeVideo(): Promise<Link> {
   const res = await http.get('/home')
   return res.data[0]
 }
