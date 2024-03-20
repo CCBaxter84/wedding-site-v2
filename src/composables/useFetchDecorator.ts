ecc8dfa80f4b6441e827e26e1ba9bfa1d431eb28
CCBaxter84 <troyceclark@gmail.com>
2024-03-20 09:08:12 -0600

    Updated composable for error handling

--- src/composables/useFetchDecorator.ts
+++ src/composables/useFetchDecorator.ts
@@ -1,14 +1,23 @@
-import { ref } from 'vue'
+import { ref, Ref } from 'vue'
 
-let isLoading = ref(false)
+export function useFetchDecorator() {
+  const isLoading = ref(false)
+  const error: Ref<any|null> = ref(null)
+  
+  async function fetchDecorator(fun: Function) {
+    isLoading.value = true
+    try {
+      await fun()
+    } catch(e: any) {
+      error.value = e
+    } finally {
+      isLoading.value = false
+    }
+  }
 
-export async function useFetchDecorator(fun: Function) {
-  isLoading.value = true
-  try {
-    await fun()
-  } catch(e) {
-    console.error(e)
-  } finally {
-    isLoading.value = false
+  return {
+    isLoading,
+    fetchDecorator,
+    error
   }
 }
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,10 @@
 <template>
   <h1>Home</h1>
+  <article>
+    <div  v-if="isLoading">Loading ...</div>
+    <div  v-else-if="error">Error</div>
+    <div  v-else>Home</div>
+  </article>
 </template>
 
 <script setup lang="ts">
@@ -9,12 +14,13 @@ import { useFetchDecorator } from '@/composables/useFetchDecorator'
 import { getHomeVideo } from '@/controllers'
 
 let homeVideo: Ref<Link|null> = ref(null)
+const { isLoading, fetchDecorator, error } = useFetchDecorator()
 
 async function setHomeVideo() {
   homeVideo.value = await getHomeVideo()
 }
 
 onMounted(async () => {
-  await useFetchDecorator(setHomeVideo)
+  await fetchDecorator(setHomeVideo)
 })
 </script>
