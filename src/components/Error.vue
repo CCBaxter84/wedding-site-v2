719b2c05c5e6f22acdf2a8df589336a9bf9f39ef
CCBaxter84 <troyceclark@gmail.com>
2024-03-20 09:12:01 -0600

    Created Error component

---
+++ src/components/Error.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <h2>Error</h2>
+    <img src="../assets/404Vader.jpeg" alt="Darth Vader">
+  </section>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
