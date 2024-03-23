9a735b8c21d4563dd34343f09fdcb4157ba607da
CCBaxter84 <troyceclark@gmail.com>
2024-03-23 13:13:55 -0600

    Added EngagementPhotos.vue w/Home as skeleton

---
+++ src/pages/EngagementPhotos.vue
@@ -0,0 +1,28 @@
+<template>
+  <AlbumContainer :isLoading="isLoading"
+                  :error="error">
+    <h1>The Clarks' Wedding</h1>
+    <EmbeddedVideo  v-if="!!homeVideo"
+                    :video="homeVideo"/>
+  </AlbumContainer>
+</template>
+
+<script setup lang="ts">
+import { onMounted, Ref, ref } from 'vue'
+import { Link } from '@/types'
+import { useFetch } from '@/composables/useFetch'
+import { getHomeVideo } from '@/controllers'
+import AlbumContainer from '@/components/AlbumContainer.vue'
+import EmbeddedVideo from '@/components/EmbeddedVideo.vue'
+
+const homeVideo: Ref<Link|null> = ref(null)
+const { isLoading, fetchDecorator, error } = useFetch()
+
+async function setHomeVideo() {
+  homeVideo.value = await getHomeVideo()
+}
+
+onMounted(async () => {
+  await fetchDecorator(setHomeVideo)
+})
+</script>
