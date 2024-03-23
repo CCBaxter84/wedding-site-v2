8f3ab0217edd65b11fe8f69809e17ddf5ded3226
CCBaxter84 <troyceclark@gmail.com>
2024-03-23 13:42:38 -0600

    Updated Photo.vue

--- src/components/Photo.vue
+++ src/components/Photo.vue
@@ -1,13 +1,18 @@
 <template>
-  <div>
-
-  </div>
+  <PhotoContainer>
+    <img :src="source.url" :alt="source.description">
+  </PhotoContainer>
 </template>
 
 <script setup lang="ts">
+import { type PropType } from 'vue'
+import PhotoContainer from './PhotoContainer.vue'
+import { Link } from '@/types'
 
+defineProps({
+  source: {
+    type: Object as PropType<Link>,
+    required: true
+  }
+})
 </script>
-
-<style scoped>
-
-</style>
