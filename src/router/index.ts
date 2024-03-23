5f2054d2ff728391fea3c6c1556149ec6aa468aa
CCBaxter84 <troyceclark@gmail.com>
2024-03-23 13:16:10 -0600

    Added route for EngagementPhotos

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,12 @@
 import { createMemoryHistory, createRouter } from 'vue-router'
 import Home from '@/pages/Home.vue'
 import OurStory from '@/pages/OurStory.vue'
+import EngagementPhotos from '@/pages/EngagementPhotos.vue'
 
 const routes = [
   { path: '/', component: Home },
-  { path: '/our-story', component: OurStory}
+  { path: '/our-story', component: OurStory },
+  { path: '/engagement-photos', component: EngagementPhotos }
 ]
 
 export default createRouter({
