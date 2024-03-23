b2451120e91fa6757c59aece1524b6860e1a4944
CCBaxter84 <troyceclark@gmail.com>
2024-03-23 13:24:24 -0600

    Created PhotoAlbum

--- src/components/PhotoAlbum.vue
+++ src/components/PhotoAlbum.vue
@@ -1,13 +1,6 @@
 <template>
-  <div>
-
-  </div>
+  <article  class="flex-column align-items-center"
+            style="margin-bottom: 5rem; padding: 0 3rem 2rem 3rem;">
+    <slot/>
+  </article>
 </template>
-
-<script setup lang="ts">
-
-</script>
-
-<style scoped>
-
-</style>
