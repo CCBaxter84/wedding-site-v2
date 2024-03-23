0e5829d4780f8f3eacc4b5b6b00075d598a39a07
CCBaxter84 <troyceclark@gmail.com>
2024-03-23 15:26:24 -0600

    Naive GettingReady

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,12 +2,13 @@ import { createMemoryHistory, createRouter } from 'vue-router'
 import Home from '@/pages/Home.vue'
 import OurStory from '@/pages/OurStory.vue'
 import EngagementPhotos from '@/pages/EngagementPhotos.vue'
+import GettingReady from '@/pages/GettingReady.vue'
 
 const routes = [
   { path: '/', component: Home },
   { path: '/our-story', component: OurStory },
   { path: '/engagement-photos', component: EngagementPhotos },
-  { path: '/getting-ready', component: EngagementPhotos }
+  { path: '/getting-ready', component: GettingReady }
 ]
 
 export default createRouter({
