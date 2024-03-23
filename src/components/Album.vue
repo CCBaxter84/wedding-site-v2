18d9b39a57ce528e007fedda16a005c91238a84d
CCBaxter84 <troyceclark@gmail.com>
2024-03-23 13:41:48 -0600

    Added Album.vue

---
+++ src/components/Album.vue
@@ -0,0 +1,20 @@
+<template>
+  <VirtualList  :dataSources="photos"
+                :dataComponent="Photo"
+                dataKey="url"/>
+</template>
+
+<script setup lang="ts">
+//@ts-ignore
+import VirtualList from 'vue3-virtual-scroll-list'
+import { type PropType } from 'vue'
+import Photo from '@/components/Photo.vue'
+import { Link } from '@/types'
+
+defineProps({
+  photos: {
+    type: Array as PropType<Link[]>,
+    required: true
+  }
+})
+</script>
