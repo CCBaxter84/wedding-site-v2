8a8f62dd8c283885bc170714df60391577895c2e
CCBaxter84 <troyceclark@gmail.com>
2024-03-23 13:10:25 -0600

    Added AlbumContainer.vue

---
+++ src/components/AlbumContainer.vue
@@ -0,0 +1,16 @@
+<template>
+  <section  v-if="!isLoading"
+            class="flex-column align-items-center"
+            style="margin-bottom: 3rem;">
+    <slot />
+  </section>
+  <Error  v-else-if="!!error"/>
+  <Loading v-else/>
+</template>
+
+<script setup lang="ts">
+import Error from './Error.vue'
+import Loading from './Loading.vue'
+
+defineProps(['error', 'isLoading'])
+</script>
