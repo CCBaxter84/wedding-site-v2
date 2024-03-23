d9c9048e988d46c64614c3e9db9cea75522264fe
CCBaxter84 <troyceclark@gmail.com>
2024-03-23 07:05:59 -0600

    Created Post.vue

---
+++ src/components/Post.vue
@@ -0,0 +1,14 @@
+<template>
+  <article>
+    <h2>{{ source.title }}</h2>
+    <img :src="source.url" :alt="source.description">
+  </article>
+</template>
+
+<script setup lang="ts">
+defineProps(['source'])
+</script>
+
+<style scoped>
+
+</style>
