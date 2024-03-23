028d7dfa0a8dad1c8bc2e5b4396c9668185ebf24
CCBaxter84 <troyceclark@gmail.com>
2024-03-23 06:24:58 -0600

    Added EmbeddedVideo

---
+++ src/components/EmbeddedVideo.vue
@@ -0,0 +1,38 @@
+<template>
+  <section class="flex-column is-full-width" >
+    <video  :src="item.url" 
+            class="video"
+            type="video/mp4"
+            ref="video"
+            autoplay
+            controls/>
+  </section>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  item: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<style scoped>
+.video {
+  max-height: 100%;
+  position: relative;
+  width: 75%;
+  height: 30rem;
+}
+@media screen and (max-width: 1020px){
+  .video {
+      width: 100%;
+  }
+}
+@media screen and (max-width: 415px){
+  .video {
+      height: 10rem;
+  }
+}
+</style>
