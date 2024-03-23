bf555fcf292d894bdc4cef0a03361f49cda3ab10
CCBaxter84 <troyceclark@gmail.com>
2024-03-23 15:32:39 -0600

    Fixed name typo

--- src/controllers/getPuppyPhotos.ts
+++ src/controllers/getPuppyPhotos.ts
@@ -1,7 +1,7 @@
 import http from '@/http'
 import { Link } from '@/types'
 
-export async function getGettingReadyPhotos(): Promise<Link[]> {
+export async function getPuppyPhotos(): Promise<Link[]> {
   const res = await http.get('/puppies')
   return res.data
 }
