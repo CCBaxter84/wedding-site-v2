f65e982ea1176fb3031e155124e7fb68c6cf57ab
CCBaxter84 <troyceclark@gmail.com>
2024-03-25 09:13:53 -0600

    Naively finished WeddingVideos

--- src/pages/WeddingVideos.vue
+++ src/pages/WeddingVideos.vue
@@ -2,16 +2,19 @@
   <AlbumContainer :isLoading="isLoading"
                   :error="error">
     <h1>Wedding Videos</h1>
-    <h2>videos go here</h2>
-    <!-- <Album  v-if="!!photos"
-            :photos="photos"/> -->
+    <template  v-for="link in links">
+      <h2>{{ link.description }}</h2>
+      <EmbeddedVideo  :video="link"
+                      style="margin-bottom: 5rem;"/>
+    </template>
   </AlbumContainer>
 </template>
 
 <script setup lang="ts">
 import { useFetch, useFetchLinksOnMount } from '@/composables'
+import AlbumContainer from '@/components/AlbumContainer.vue'
+import EmbeddedVideo from '@/components/EmbeddedVideo.vue'
 
 const { isLoading, error } = useFetch()
 const { links } = useFetchLinksOnMount()
-console.log(links)
 </script>
