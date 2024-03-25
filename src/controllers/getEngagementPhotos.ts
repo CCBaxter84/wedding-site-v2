f8d3f07e08187b6d9ef75f6ec8913e7957915e64
CCBaxter84 <troyceclark@gmail.com>
2024-03-25 06:15:22 -0600

    Removed Engagement

--- src/controllers/getEngagementPhotos.ts
+++
@@ -1,7 +0,0 @@
-import http from '@/http'
-import { Link } from '@/types'
-
-export async function getEngagementPhotos(): Promise<Link[]> {
-  const res = await http.get('/engagement')
-  return res.data
-}
--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,5 +1,4 @@
 export * from './getHomeVideo'
-export * from './getEngagementPhotos'
 export * from './getGettingReadyPhotos'
 export * from './getPuppyPhotos'
 export * from './getCeremonyPhotos'
