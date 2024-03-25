e7c9e8d29e15943e01d0c6648741d181df64b04d
CCBaxter84 <troyceclark@gmail.com>
2024-03-25 07:28:23 -0600

    Refactored PhotoAlbum to use composable

--- src/pages/PhotoAlbum.vue
+++ src/pages/PhotoAlbum.vue
@@ -2,32 +2,22 @@
   <AlbumContainer :isLoading="isLoading"
                   :error="error">
     <h1>{{ route.meta.title }}</h1>
-    <Album  v-if="!!photos"
-            :photos="photos"/>
+    <Album  v-if="!!links"
+            :photos="links"/>
   </AlbumContainer>
 </template>
 
 <script setup lang="ts">
-import { onMounted, Ref, ref, computed, watch } from 'vue'
+import { computed, watch } from 'vue'
 import { useRoute } from 'vue-router'
-import { Link } from '@/types'
-import { ApiPaths } from '@/types/enums'
-import { useFetch } from '@/composables'
-import { getPhotoAlbum } from '@/controllers'
+import { useFetch, useFetchLinksOnMount } from '@/composables'
 import AlbumContainer from '@/components/AlbumContainer.vue'
 import Album from '@/components/Album.vue'
 
-const photos: Ref<Link[]|null> = ref(null)
-const { isLoading, fetchDecorator, error } = useFetch()
 const route = useRoute()
 const path = computed(() => route.path)
+const { isLoading, error } = useFetch()
+const { links, setLinks } = useFetchLinksOnMount()
 
-async function setPhotos() {
-  photos.value = await getPhotoAlbum(path.value as ApiPaths)
-}
-
-onMounted(async () => {
-  await fetchDecorator(setPhotos)
-})
-watch(path, setPhotos)
+watch(path, setLinks)
 </script>
