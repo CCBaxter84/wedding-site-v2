a0539c6d682a3fa1c8af329adcf8de2f342d0b4d
CCBaxter84 <troyceclark@gmail.com>
2024-03-26 07:27:03 -0600

    Added mobile p tag CSS

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,7 +1,7 @@
 <template>
   <PhotoContainer>
     <h2>{{ source.title }}</h2>
-    <p  style="padding: 0 2rem;">{{ source.text }}</p>
+    <p>{{ source.text }}</p>
     <img :src="source.url" :alt="source.description">
   </PhotoContainer>
 </template>
@@ -11,3 +11,14 @@ import PhotoContainer from './PhotoContainer.vue'
 
 defineProps(['source'])
 </script>
+
+<style scoped>
+p {
+  padding: 0 2rem;
+}
+@media screen and (max-width: 1025px) {
+  p {
+    padding: 0;
+  }
+}
+</style>
